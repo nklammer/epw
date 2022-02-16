37792a91f7998d77753b68f97e3398de1fa26fe5
Noah Klammer <57372933+nklammer@users.noreply.github.com>
2022-02-16 17:46:09 -0500

    simple execution of NYC data to csv with headers

--- epw2csv.py
+++ epw2csv.py
@@ -1,3 +1,13 @@
 from epw import epw
 a = epw()
-a.read()
+a.read('USA_NY_New.York-Central.Park.725033_TMY3.epw')
+
+
+d = a.headers
+# print(d)
+
+df = a.dataframe
+
+# print(df.head())
+
+df.to_csv('NYC_epw.csv')
